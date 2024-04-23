3bd643835076847d9965f0373b0d3c2af7429a94
code craftsmen <123401258+maheshkatyayan@users.noreply.github.com>
2024-04-23 11:42:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import Clientlogin from "./component/clientlogin.js"; 
 import Question from "./component/Question.js";
-import Adminlogin from "./component/admin.js";
+import Adminlogin from "./component/admin.jsx";
 import Home from "./component/Home.js";
 import Timer from "./component/Timer.js";
 import Showqestion from "./component/showquestion.js";
