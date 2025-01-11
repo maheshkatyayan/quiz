eed798fbb1eb1362954f72b9a6cf933814d40ae4
code craftsmen <123401258+maheshkatyayan@users.noreply.github.com>
2025-01-12 01:14:44 +0530

    Update tailwind.config.js

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
@@ -7,4 +7,6 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
+
+export default config;
